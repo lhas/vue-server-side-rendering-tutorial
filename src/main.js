c1bd684477bf11ab5f25cfcdf7052569faccd1ce
lhas <luizhrqas@gmail.com>
2017-01-18 12:37:22 -0200

    Added Routes

--- src/main.js
+++ src/main.js
@@ -2,10 +2,29 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Foo from './components/foo'
+import Bar from './components/bar'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', redirect: '/foo' },
+  { path: '/foo', component: Foo },
+  { path: '/bar', template: Bar }
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes // short for routes: routes
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App }
+  components: { App },
+  router
 })
